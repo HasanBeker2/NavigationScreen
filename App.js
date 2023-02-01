db03284615ff446839792642e36ea0dcc2523034
hasanbeker22 <hasanbeker22@gmail.com>
2023-02-01 22:13:39 +0100

    navigation app

--- App.js
+++ App.js
@@ -1,20 +1,32 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import * as React from 'react';
+import { View, StyleSheet } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+
+import WelcomeScreen from "./Screens/WelcomeScreen";
+import MenuScreen from "./Screens/MenuScreen";
+
+const Stack = createNativeStackNavigator();
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen name="Welcome" component={WelcomeScreen} />
+        <Stack.Screen name="Menu" component={MenuScreen} />
+      </Stack.Navigator>
+    </NavigationContainer>
+      
   );
 }
 
 const styles = StyleSheet.create({
-  container: {
+  headerContainer: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: '#333333',
+  },
+
+  footerContainer: {
+    backgroundColor: '#333333',
   },
 });
